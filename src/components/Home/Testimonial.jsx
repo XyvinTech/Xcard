1798c22fd6c9b755c67eeb873576643aa046d2f5
Fasilfaz <fazsby2001@gmail.com>
2025-10-06 12:34:58 +0530

    + added

--- src/components/Home/Testimonial.jsx
+++ src/components/Home/Testimonial.jsx
@@ -16,13 +16,13 @@ const testimonials = [
   {
     img: person2,
     name: "Dr Asaad Eltoum",
-    phone: "971555277760",
+    phone: "+971555277760",
     location: "Saudi Arabia",
   },
   {
     img: person4,
     name: "Mohammed Almortada",
-    phone: "966558040673",
+    phone: "+966558040673",
     location: "Egypt",
   },
   {
@@ -34,7 +34,7 @@ const testimonials = [
   {
     img: person5,
     name: "Abied Abdelgawe",
-    phone: "249912200031",
+    phone: "+249912200031",
     location: "Sudan",
   },
 ];
